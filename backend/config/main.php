0b0ad805109d4e2c6567367179bedfb312aa2230
thohirah <thohirh9@gmail.com>
2017-09-14 13:48:01 +0700

    Homecontroller + index medthod

--- backend/config/main.php
+++ backend/config/main.php
@@ -10,6 +10,7 @@ return [
     'id' => 'app-backend',
 	'name'=>'Basic App',
     'basePath' => dirname(__DIR__),
+    'defaultRoute' => 'home/index',
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
     'modules' => [],
