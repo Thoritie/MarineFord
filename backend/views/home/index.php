eada0008c68ba72a27ba64b8ff77fc4152bca84f
DESKTOP-QQ4UMN4\asus <Pongsatorn698@gmail.com>
2017-09-16 00:39:46 +0700

    เพิ่มเติมหน้า Home(Index)

--- backend/views/home/index.php
+++ backend/views/home/index.php
@@ -5,13 +5,54 @@
     <title>Marine Ford</title>
   </head>
   <body>
+    <!-- <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header"><h1>MarineFord</h1></div>
+          <ul class="navbar-header">
+            <li role="presentation" class="active"><a href="#">Home</a></li>
+            <li role="presentation"><a href="#">Profile</a></li>
+            <li role="presentation"><a href="#">Messages</a></li>
+          </ul>
+
+
+
+      </div>
+    </nav> -->
+
+
     <nav class="navbar navbar-default navbar-fixed-top">
       <div class="container">
-         <h1>MarineFord</h1>
-      </div>
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-header" href="#"><h1>MarineFord</h1></a>
+        </div>
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><h4><span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;&nbsp;Home </h4></a></li>
+            <li><a href="#"><h4><span class="glyphicon glyphicon-search" aria-hidden="true"></span>&nbsp;&nbsp; List </h4></a></li>
+            <li><a href="#"><h4><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>&nbsp;&nbsp; จอง </h4></a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </div>
     </nav>
+
+
+
+
+
+
+
   <br><br><br>
-  <img src="http://californiadriving.co.kr/wp-content/uploads/2015/04/The-Avila-Beach-pier-1600-900.png" class="img-responsive" alt="Responsive image">
+  <img src="https://2.bp.blogspot.com/-ttHvhLwGki4/V7pdThnOU0I/AAAAAAACmlM/SOA-Oi0iLwgGbRJ4HbDfYYww0c8MNusMwCLcB/s1600/sailing-boat-1593613.jpg" class="img-responsive" alt="Responsive image">
+  <p class="text-muted">...</p>
   <br><br>
   <div class="container">
       <div class="panel panel-default">
