0d5536f59e7fd2283cbeb944b34051e192c3ca54
thohirah <thohirh9@gmail.com>
2017-09-16 16:31:23 +0700

    Test Reservation :beer:

--- backend/controllers/ReserController.php
+++ backend/controllers/ReserController.php
@@ -37,5 +37,47 @@ class ReserController extends Controller
 
     }
 
+    public function actionTestreser()
+    {
+
+        $this->layout = "@backend/themes/adminlte/layouts/index";
+        return $this->render('testreser');
+
+
+    }
+
+    public function actionSave(){
+    	// $request = Yii::$app->request;
+        //
+    	// $id = $request->get('id',null);
+    	// $name = $request->get('name',null);
+    	// $gender = $request->get('gender',null);
+    	// $email = $request->get('email',null);
+        // $phone_number = $request->get('phone_number',null);
+        //
+    	// $baseUrl = \Yii::getAlias('@web');
+        //
+    	// $model;
+    	// if($id == null){
+    	// 	//create
+    	// 	$model = new Lecturer();
+    	// }else{
+    	// 	//update
+    	// 	$model = Lecturer::findOne($id);
+    	// }
+        //
+    	// $model->name = $name;
+    	// $model->gender = $gender;
+    	// $model->email = $email;
+        // $model->phone_number = $phone_number;
+    	// if($model->save()){
+    	// 	echo "success";
+    	// }else{
+    	// 	echo "unsuccess";
+    	// }
+        //
+    	// return $this->redirect($baseUrl."/lecturer/index");
+    }
+
 
 }
