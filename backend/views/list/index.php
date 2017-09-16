536660865be064d7c00e8b5eb710e3bfd8c221aa
Tannnnnn <tan_sweetmulet@hotmail.com>
2017-09-17 05:49:36 +0700

    search list

--- backend/views/list/index.php
+++ backend/views/list/index.php
@@ -1,40 +1,40 @@
 <?php
-use backend\models\Boat;
+use yii\widgets\LinkPager;
 $this->title = 'List Boat';
 $this->params['breadcrumbs'][] = $this->title;
-
 $baseUrl = \Yii::getAlias('@web');
 ?>
 
 <div class="container">
-	<h1>List Boat</h1>
-	<div class="row" align="right">
-		<div class="col-lg-4" >
-		    <div class="input-group" >
-			   <input type="text" class="form-control" placeholder="Search for...">
-			   <span class="input-group-btn">
-			   <button class="btn btn-info" type="button" value=" ">Go!</button></span>
-		    </div>
-	  	</div>
-	</div>
-	<br>
-	<?php foreach ($result as $var){ ?>
-		<div class="col-sm-6 col-md-4">	
-			<div class="thumbnail" >
-				<img class="img-responsive img-thumbnail" src=""
-                     alt=" " style="width:250px;height:250px";>
-                <div class="caption">
-                	<h3 class="card-title">name :<?=$var['name']?></h3>
-					<p class="card-text">type :<?=$var['type']?></p>
-					<p class="card-text">max of seat :<?=$var['maxseat']?></p>
-					<button class="btn btn-primary" role="button"
-					href="<?=$baseUrl."/lecturer/save?id=".$var['_id']?>">book</button>
-                </div>
-				
-				<br><br>
-			</div>	
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="pull-left">Boat List</h4>
+			<form class="input-group" action="" method="get">
+				<input type="text" name="search" value="<?php echo $input; ?>" placeholder="Search.." class="form-control pull-right" style="width:200px;">
+				<div class="input-group-btn">
+					<button class="btn btn-primary btn-sm" type="submit">
+						<i class="glyphicon glyphicon-search"></i>
+					</button>
+				</div>
+			</form>
 		</div>
-
-	<?php }?>
+		<div class="panel panel-body">
+			<?php foreach ($result as $var){ ?>
+			  <div class="col-sm-6 col-md-4">
+			    <div class="thumbnail" >
+			      <img class="img-responsive img-thumbnail" src="" alt=" " style="width:250px;height:250px";>
+			      <div class="caption">
+			        <h3 class="card-title">name :<?=$var['name']?></h3>
+			          <p class="card-text">type :<?=$var['type']?></p>
+			          <p class="card-text">max of seat :<?=$var['maxseat']?></p>
+			        <button class="btn btn-primary" role="button" href="<?=$baseUrl."/reser/index" ?>">book</button>
+			      </div>
+			    </div>
+			  </div>
+			<?php }?>
+		</div>
+		<div class="text-center">
+			<?= LinkPager::widget(['pagination' => $pagination]) ?>
+		</div><br><br>
+	</div>
 </div>
-
