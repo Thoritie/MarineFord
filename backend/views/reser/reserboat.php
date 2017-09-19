c5ef163f71cb9022e64d8d1907c292af8d1df83f
NEXT-L5GT2RS78Q\NEXT Speed <dome19962539@gmail.com>
2017-09-19 21:02:52 +0700

    เปลี่ยนที่เเสดงชื่อเรือ

--- backend/views/reser/reserboat.php
+++ backend/views/reser/reserboat.php
@@ -2,8 +2,8 @@
 use yii\helpers\Html;
 $baseUrl=\Yii::getAlias('@web');
  ?>
-
-      <br><br><br><br>
+      <br><br><h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ชื่อเรือ :<?=$model ['name'] ?></h1>
+      <br>
 
     <div class="container">
       <div class="panel panel-default">
@@ -13,38 +13,13 @@ $baseUrl=\Yii::getAlias('@web');
               <input type="hidden" name="id" value="<?=$model['boat_id'] ?>">
 
               <div class="col-md-5"><img src="http://wallpapers.wallpapersdepo.net/free-wallpapers/3123/Great-Voyage.jpg" alt="..." class="img-thumbnail"></div>
-              <div class="col-md-6"><div class="form-group">
-                  <h1>ชื่อเรือ : <?=$model['name'] ?></h1>
-                </div>
+              <div class="col-md-6">
                 <div class="form-group">
                   <h1>ประเภท : <?=$model['type'] ?></h1>
                 </div>
-
                 <div class="form-group">
                       <h1>จำนวนที่นั่ง : <?=$model['maxseat'] ?></h1>
                 </div>
-
-                    <!-- <div class="form-group">
-                        <label  class="col-sm-2 control-label">วันที่จอง</label>
-                            <div class="col-xs-2 col-sm-4"><div class="col-sm-10">
-                                <input type="text" class="form-control" id="inputtext" placeholder="2013-12-14" name="rentdate">
-                              </div>
-                            </div>
-                              <div class="form-group">
-                                <label  class="col-sm-2 control-label">ถึงวันที่</label>
-                                  <div class="col-xs-2 col-sm-4"><div class="col-sm-10">
-                                        <input type="text" class="form-control" id="inputtext" placeholder="2013-12-14" name="backdate">
-                                      </div>
-                                  </div>
-                              </div>
-                    </div><br><br><br> -->
-                        <!-- <div class="form-group">
-                            <label  class="col-sm-2 control-label">ระยะทาง</label>
-                                <div class="col-xs-2 col-sm-4"><div class="col-sm-10">
-                                    <input type="text" class="form-control" id="inputtext" placeholder="text" name="distance">
-                                  </div>
-                                </div>
-                      </div> -->
                     </div>
                 </div>
                 </div>
@@ -53,22 +28,8 @@ $baseUrl=\Yii::getAlias('@web');
 			                ชื่อลูกค้า <input type="text" name="cusname">
 			                ระยะทาง <input type="text" name="distance">
 			                วันไป <input type="date" name="rentdate">
-			                  วันกลับ <input type="date" name="backdate">
-       			<input type="hidden" name="boat_id" value="<?=$model['boat_id'] ?>">        
+			                วันกลับ <input type="date" name="backdate">
+       			<input type="hidden" name="boat_id" value="<?=$model['boat_id'] ?>">
                 <input type="submit" value="บันทึก">
               </form>
             </div>
-                    <!-- <div class="panel panel-default">
-                      <div class="panel-body">
-                        <div class="form-group">
-                            <label  class="col-sm-1 control-label">ชื่อลูกค้า</label>
-                                <div class="col-xs-5 col-sm-5"><div class="col-sm-10">
-                                    <input type="text" class="form-control" id="inputtext" placeholder="text" name="cusname">
-                                  </div>
-                                </div>
-                        </div>
-
-
-                        <br><br><br>
-                      </div>
-                    </div> -->
